d4b73809e42c782a0581f85a0c5567ad36dc2ee6
isato7676 <isaac.sanchez.torres@tajamar365.com>
2025-06-22 17:56:07 +0200

    Primer commit - proyecto base GeoMark

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>GeoMark</h1>
+      <p>Bienvenido al juego de geolocalización educativa.</p>
+    </div>
+  );
+}
+
+export default App;
